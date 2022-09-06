9f17b55f9726fede163909dfcff8bda35f8e1eb6
BautyGarcia <76719703+Drethtar@users.noreply.github.com>
2022-09-06 10:15:45 -0300

    fix: fixed itemList querys

--- service/itemList_Service.js
+++ service/itemList_Service.js
@@ -6,10 +6,10 @@ class itemListService {
 
         const { nombreEvento } = listInfo
 
-        const itemsToBring = await prisma.consasTraer.findMany({
+        const itemsToBring = await prisma.cosasTraer.findMany({
             where: {
                 evento: {
-                    nombreEvento
+                    nombre: nombreEvento
                 }
             },
             select: {
@@ -25,10 +25,10 @@ class itemListService {
             
         const { nombreEvento } = listInfo
     
-        const itemsBrought = await prisma.consasTraer.findMany({
+        const itemsBrought = await prisma.cosasTraidas.findMany({
             where: {
                 evento: {
-                    nombreEvento
+                    nombre: nombreEvento
                 }
             },
             select: {
@@ -48,7 +48,8 @@ class itemListService {
     async setItemsToBring(listInfo) {
 
         const { nombreEvento, nombreHost, nombreObjeto, cantidadObjeto } = listInfo
-
+        
+        //Busco que exista un evento de ese usuario con ese nombre
         const event = await prisma.eventos.findFirst({
             where: {
                 nombre: nombreEvento,
@@ -62,16 +63,42 @@ class itemListService {
             throw new Error("Event not found")
         }
 
+        //Me fijo si ya hay un registro de ese objeto
         const checkList = await prisma.cosasTraer.findFirst({
             where: {
-                nombreObjeto: nombreObjeto
+                nombreObjeto
             }
         })
 
-        if (!!checkList) {
-            throw new Error("Item already exists")
+        if (checkList) {
+            //Si existe un registro de ese objeto, sumo la cantidad
+            const newCheckList = await prisma.cosasTraer.update({
+                where: {
+                    id: checkList.id
+                },
+                data: {
+                    cantidad: {
+                        increment: cantidadObjeto
+                    }
+                }
+            })
+
+            if (newCheckList.cantidad <= 0){
+                //Si me ingresan un numero negativo, y la resta da 0 o menos, elimino el registro
+                const deleteCheckList = await prisma.cosasTraidas.delete({
+                    where: {
+                        evento: {
+                            nombre: nombreEvento
+                        },
+                        nombreObjeto
+                    }
+                })
+
+                return deleteCheckList
+            }
         }
 
+        //Si no existe el registro de ese objeto, lo creo
         const newList = await prisma.cosasTraer.create({
             data: {
                 nombreObjeto,
@@ -88,9 +115,9 @@ class itemListService {
     }
 
     async setItemsBrought(listInfo) {
-        
         const { nombreEvento, nombreObjeto, cantidadObjeto, nombreUsuario } = listInfo
 
+        //Me fijo si el evento existe
         const event = await prisma.eventos.findFirst({
             where: {
                 nombre: nombreEvento
@@ -101,6 +128,7 @@ class itemListService {
             throw new Error("Event not found")
         }
 
+        //Me fijo si el usuario esta en el evento
         const eventParticipant = await prisma.usuarioEventos.findFirst({
             where: {
                 usuario: {
@@ -116,54 +144,123 @@ class itemListService {
             throw new Error("You are not part of this event")
         }
 
+        //Me fijo si ese objeto esta en la lista de cosas para traer
         const checkList = await prisma.cosasTraer.findFirst({
             where: {
                 nombreObjeto
             }
         })
 
+        if (cantidadObjeto > checkList.cantidad) {
+            throw new Error("You brought more than you were supposed to")
+        }
+
         if (!checkList) {
             throw new Error("Item not found")
         }
 
-        const newList = await prisma.cosasTraer.update({
+        //Me fijo si el usuario ya trajo ese objeto
+        const sumItems = await prisma.cosasTraidas.findFirst({
             where: {
-                id: checkList.id
-            },
-            data: {
-                cantidad: {
-                    decrement: cantidadObjeto
+                nombreObjeto,
+                usuario: {
+                    nombreUsuario
                 }
             }
         })
 
-        const newBroughtList = await prisma.cosasTraidas.create({
-            data: {
-                nombreObjeto,
-                cantidad: cantidadObjeto,
-                evento: {
-                    connect: {
-                        nombre: nombreEvento
+        //Si no trajo ese objeto, creo el registro en cosasTraidas y cambio la cantidad en cosasTraer
+        if (!sumItems) {
+            const sumList = await prisma.cosasTraidas.create({
+                data: {
+                    nombreObjeto,
+                    cantidad: cantidadObjeto,
+                    usuario: {
+                        connect: {
+                            nombreUsuario
+                        }
+                    },
+                    evento: {
+                        connect: {
+                            nombre: nombreEvento
+                        }
                     }
+                    
                 },
-                usuario: {
-                    connect: {
-                        nombreUsuario
+                select: {
+                    nombreObjeto: true,
+                    cantidad: true,
+                    usuario: {
+                        select: {
+                            nombreUsuario: true
+                        }
                     }
                 }
-            },
-            select: {
-                nombreObjeto: true,
-                cantidad: true,
-                usuario: {
-                    select: {
-                        nombreUsuario: true
+            })
+
+            if (sumList.cantidad <= 0){
+
+                const deleteList = await prisma.cosasTraidas.delete({
+                    where: {
+                        id: sumList.id
                     }
-                }
+                })
+
+                return deleteList
+
+            } else {
+                const newList = await prisma.cosasTraer.update({
+                    where: {
+                        id: checkList.id
+                    },
+                    data: {
+                        cantidad: {
+                            decrement: cantidadObjeto
+                        }
+                    }
+                })
+    
+                return newList
             }
-        })    
-        
-        return newBroughtList
+
+        } else {
+            //Si ya lo trajo, sumo la cantidad en cosasTraidas y resto en cosasTraer
+            const sumList = await prisma.cosasTraidas.update({
+                where: {
+                    id: sumItems.id
+                },
+                data: {
+                    cantidad: {
+                        increment: cantidadObjeto
+                    }
+                }
+            })
+
+            if (sumList.cantidad <= 0){
+                await prisma.cosasTraidas.delete({
+                    where: {
+                        id: sumList.id
+                    }
+                })
+
+                return new Error("You inserted a nonesense brought value")
+
+            } else {
+                const newList = await prisma.cosasTraer.update({
+                    where: {
+                        id: checkList.id
+                    },
+                    data: {
+                        cantidad: {
+                            decrement: cantidadObjeto
+                        }
+                    }
+                })
+            }
+
+            return sumList
+            
+        }
     }
 }
 
